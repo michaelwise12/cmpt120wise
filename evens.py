68ffcc9a7ce755c121542fd009ae593aa1c99e17
michaelwise12 <Michael.Wise1@edu>
2019-09-17 17:33:38 -0400

    Solution to evens loop exercise.

---
+++ evens.py
@@ -0,0 +1,16 @@
+#evens.py
+
+def main():
+    for i in range(10):
+        print((i+1)*2)
+
+main()
+
+#evens.py Alternate Solution
+print("Also accomplished through an alternate solution:")
+
+def alt():
+    for i in range(2, 21, 2):
+        print(i)
+
+alt()
