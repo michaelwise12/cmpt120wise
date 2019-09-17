1ab1d99c0ba9e2843c7b1ee094c01f02e49ff6f2
michaelwise12 <Michael.Wise1@edu>
2019-09-17 17:48:11 -0400

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,12 @@
+#madlib.py - This program serves as a MadLibs.
+#Author: Michael Wise
+
+def main():
+    animal = input("Enter a type of animal:")
+    verb = input("Enter a verb in the past tense:")
+    name = input("Enter a name of a person:")
+    adjective = input("Enter an adjective:")
+
+    print("My pet", animal, "just", verb, "my friend named", name, "in the", adjective, "bathroom.")
+
+main()
