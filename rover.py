7fa8b8ad63249271de3b64c8cfa3e8c3af3f4f20
michaelwise12 <Michael.Wise1@edu>
2019-09-17 17:22:41 -0400

    Solution to Curiosity rover exercise.

---
+++ rover.py
@@ -0,0 +1,13 @@
+#This program calculates how long it takes a photo from the Curiosity rover to reach NASA
+def main():
+    
+    speed_light = 186000
+    distance = 34000000
+    time_in_sec = distance / speed_light
+
+    print("It takes approximately", time_in_sec, "seconds for NASA to recieve photos from Curiosity at Earth's closest orbit.")
+
+main()
+
+      
+      
