2664119bf6aa8cba89045baa2c3d985c18f8fc2b
michaelwise12 <Michael.Wise1@edu>
2019-10-01 19:43:45 -0400

    Added even more improvements to credentials.py to make it even more modular.

--- credentials.py
+++ credentials.py
@@ -12,25 +12,35 @@ def getName():
 def username():
     global uname
     uname = first + "." + last
+    uname = uname.lower()
     return uname
 
 def password():
+    global passwd
     passwd = input("Create a new password: ")
 
+def isPass(passwd):
     while len(passwd) < 8:
         print("Fool of a Took! That password is feeble!")
         passwd = input("Create a new password: ")
         if len(passwd) >= 8:
             break
-    return passwd
 
+    if passwd != passwd.lower() and passwd != passwd.upper():
+        return True
+    else:
+        return False
+  
 def main():
     getName()
     username()
     password()
-
+    while isPass(passwd) == False:
+        print("Your password needs to contain at least one upper and one lower case letter.")
+        if isPass(passwd) == True:
+            break
     print("The force is strong in this one…")
     print("Account configured. Your new email address is",
-    uname + "@marist.edu")
+    uname + "@marist.edu")           
 
 main()
