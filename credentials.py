9949dd19a23db366b3d5a672a5b4ef7494b518b6
michaelwise12 <Michael.Wise1@edu>
2019-10-01 18:27:26 -0400

    Refactored the program by creating functions in the code.

--- credentials.py
+++ credentials.py
@@ -2,22 +2,34 @@
 # Lab #5 – Working with Strings and Functions
 # Author: Michael Wise
 # Created: 2019-10-01
-def main():
-    # get user's first and last names
+
+def getName():
+    global first,last
     first = input("Enter your first name: ")
     last = input("Enter your last name: ")
-    # Modified to generate a Marist-style username
+    return first,last
+
+def username():
+    global uname
     uname = first + "." + last
-    # ask user to create a new password
+    return uname
+
+def password():
     passwd = input("Create a new password: ")
-    # Modified to ensure the password has at least 8 characters
+
     while len(passwd) < 8:
         print("Fool of a Took! That password is feeble!")
         passwd = input("Create a new password: ")
         if len(passwd) >= 8:
             break
+
+def main():
+    getName()
+    username()
+    password()
+
     print("The force is strong in this one…")
     print("Account configured. Your new email address is",
     uname + "@marist.edu")
-    
+
 main()
