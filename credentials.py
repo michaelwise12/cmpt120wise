6edd0f0bea75a450b301505b83ff2da25d9a08d2
michaelwise12 <Michael.Wise1@edu>
2019-10-02 17:41:53 -0400

    Final version of the credentials.py program.

--- credentials.py
+++ credentials.py
@@ -1,23 +1,21 @@
 # CMPT 120 Intro to Programming
-# Lab #5 – Working with Strings and Functions
+# Lab #5 – Working with Strings and Functions (FINAL VERSION)
 # Author: Michael Wise
 # Created: 2019-10-01
 
 def getName():
-    global first,last
     first = input("Enter your first name: ")
     last = input("Enter your last name: ")
     return first,last
 
-def username():
-    global uname
-    uname = first + "." + last
+def username(names):
+    uname = names[0] + "." + names[1]
     uname = uname.lower()
     return uname
 
 def password():
-    global passwd
     passwd = input("Create a new password: ")
+    return passwd
 
 def isPass(passwd):
     while len(passwd) < 8:
@@ -32,9 +30,9 @@ def isPass(passwd):
         return False
   
 def main():
-    getName()
-    username()
-    password()
+    names = getName()
+    uname = username(names)
+    passwd = password()
     while isPass(passwd) == False:
         print("Your password needs to contain at least one upper and one lower case letter.")
         if isPass(passwd) == True:
