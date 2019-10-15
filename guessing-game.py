2cf6f6d20a8b3d67b554dac2220cdb98334d245d
michaelwise12 <Michael.Wise1@edu>
2019-10-15 17:47:16 -0400

    Initial version of a simple guessing game.

---
+++ guessing-game.py
@@ -0,0 +1,19 @@
+# guessing-game.py
+# Author: Michael Wise
+# CMPT 120 Lab 5
+
+def main():
+
+    while True:
+        print("I am thinking of an animal.")
+        guess = input("Guess the name of the animal:")
+        
+        if guess == "zebra":
+            print("You guessed correctly!")
+            break
+        
+        else:
+            print("That is incorrect! Try again!")
+            print()
+                
+main()
