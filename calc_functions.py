dc99a22bb241f5a4c3c1bd779196fe722134202a
michaelwise12 <Michael.Wise1@edu>
2019-10-15 20:32:08 -0400

    Fixed some bugs & added multiplication/division to calc. program.

--- calc_functions.py
+++ calc_functions.py
@@ -1,18 +1,35 @@
 # calc_functions.py
 # CMPT 120: Calculator Project
 # Author: Michael Wise
-#
+# v0.1
+
 import math
 
 def calculator():
     equation = input("Enter an equation using (+,-,*,/)")
-    eqlist = equation.split(" ")
+    eqlist = equation.split(" ")  # splits the string into a list
 
     while len(eqlist) > 1:
-        for i in range(1, len(eqlist), 2):
+
+        for i in range(1, len(eqlist), 2):   # ensures that it only checks for operators
+            
+            # Multiplication / Division
+            if '*' in eqlist or '/' in eqlist:
+
+                if eqlist[i] == '*':
+                    result = float(eqlist[i-1]) * float(eqlist[i+1])
+                    del eqlist[i-1:i+2]
+                    eqlist.insert(i-1, result)
+                    break
+      
+                elif eqlist[i] == '/':
+                    result = float(eqlist[i-1]) / float(eqlist[i+1])
+                    del eqlist[i-1:i+2]
+                    eqlist.insert(i-1, result)
+                    break
 
             # Addition / Subtraction
-            if '+' in eqlist or '-' in eqlist:
+            elif '+' in eqlist or '-' in eqlist:
                 
                 if eqlist[i] == '+':
                     result = float(eqlist[i-1]) + float(eqlist[i+1])
