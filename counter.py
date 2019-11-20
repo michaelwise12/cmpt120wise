912d5997ddc17b7823406d578f4a9fe84cce3eaa
michaelwise12 <Michael.Wise1@edu>
2019-11-19 21:22:47 -0500

    Overrode the increment method in the DecrementingCounter2 class.

--- counter.py
+++ counter.py
@@ -15,8 +15,15 @@ class Counter:
 
 class DecrementingCounter2(Counter):
     """Simple counter that can be incremented, decremented, and cleared."""
+
+    def increment(self):
+        """Increment counter by 2."""
+        self.count += 2
+
     def decrement(self):
         """Decrement counter by 1."""
-        self.count -= 1
+        self.count -= 1
+        
+
 
 
